71ac3a479cb41d96f7a78ee98aa65a219d986c03
Vic Ramon <vic@vicramon.com>
2014-01-31 14:51:37 -0500

    Use history api

--- config/router.js.es6
+++ config/router.js.es6
@@ -1,11 +1,5 @@
 var Router = Ember.Router.extend({
-  // Uncomment to change Ember's router to use the
-  // HTML5 History API
-  // Please note that not all browsers support this!
-  // You will also need to uncomment the greedy route matcher
-  // in config/routes.rb
-
-  // location: 'history'
+  location: 'history'
 });
 
 Router.map(function() {
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ AppkitTest::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get '*path', :to => 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
